8eabe4a8d879363b02f607199afbfbdcc01f6944
Andrii Mutylo <amutylo@webcodin.com>
2019-02-01 17:13:20 +0200

    added graphql func

--- app.js
+++ app.js
@@ -5,32 +5,56 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
 	'/graphql',
 	graphqlHttp({
 		schema: buildSchema(`
-      type RootQuery {
-        events: [String!]!
-      }
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
 
-      type RootMutation {
-        createEvent(name:String): String
-      }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
 
-      schema {
-        query: RootQuery,
-        mutation: RootMutation
-      }
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
     `),
 		rootValue: {
 			events: () => {
-				return ['Romantic', 'cooking', 'Sailing', 'All-night coding'];
+				return events;
 			},
-			createEvent: arg => {
-				const eventName = arg.name;
-				return eventName;
+			createEvent: args => {
+				const event = {
+					_id: Math.random().toString(),
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: +args.eventInput.price,
+					date: args.eventInput.date,
+				};
+				events.push(event);
+				return event;
 			},
 		},
 		graphiql: true,
