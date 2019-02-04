0ad2cf3ac701e124454219aa61d58af28b89731c
Andrii Mutylo <amutylo@webcodin.com>
2019-02-04 16:28:31 +0200

    code style

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const Event = require('./models/event');
 const User = require('./models/user');
-const bcrypt = require('bcryptjs');
 
 const app = express();
 
