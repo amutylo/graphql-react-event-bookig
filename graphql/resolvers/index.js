9943567b8eaf672973ae9051bc5f7981d22890e7
Andrii Mutylo <amutylo@webcodin.com>
2019-03-14 17:32:08 +0200

    refactor resolver file

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,13 +3,25 @@ const bcrypt = require('bcryptjs');
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
+const { dateToString } = require('../helpers/date');
 
 const transforEvent = event => {
 	return {
 		...event._doc,
 		_id: event.id,
-		date: new Date(event._doc.date).toISOString(),
+		date: dateToString(event._doc.date),
 		creator: user.bind(this, event.creator),
+	};
+};
+
+const transformBooking = booking => {
+	return {
+		...booking._doc,
+		_id: booking.id,
+		user: user.bind(this, booking._doc.user),
+		event: singleEvent.bind(this, booking._doc.event),
+		createdAt: dateToString(booking._doc.createdAt),
+		updatedAt: dateToString(booking._doc.updatedAt),
 	};
 };
 
@@ -61,14 +73,7 @@ module.exports = {
 		try {
 			const bookings = await Booking.find();
 			return bookings.map(booking => {
-				return {
-					...booking._doc,
-					_id: booking.id,
-					user: user.bind(this, booking._doc.user),
-					event: singleEvent.bind(this, booking._doc.event),
-					createdAt: new Date(booking._doc.createdAt).toISOString(),
-					updatedAt: new Date(booking._doc.updatedAt).toISOString(),
-				};
+				return transformBooking(booking);
 			});
 		} catch (err) {
 			throw err;
@@ -127,14 +132,7 @@ module.exports = {
 			event: fetchedEvent,
 		});
 		const result = await booking.save();
-		return {
-			...result._doc,
-			_id: result.id,
-			user: user.bind(this, booking._doc.user),
-			event: singleEvent.bind(this, booking._doc.event),
-			createdAt: new Date(result._doc.createdAt).toISOString(),
-			updatedAt: new Date(result._doc.updatedAt).toISOString(),
-		};
+		return transformBooking(result);
 	},
 	cancelBooking: async args => {
 		try {
