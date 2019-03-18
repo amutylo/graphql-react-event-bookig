5f099bf28dd329b404c84fe247b046a65fc80b22
Andrii Mutylo <amutylo@webcodin.com>
2019-03-18 17:51:48 +0200

    added login/signup form

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+	if (req.method === 'OPTIONS') {
+		return res.sendStatus(200);
+	}
+	next();
+});
 app.use(isAuth);
 
 app.use(
@@ -25,7 +34,7 @@ mongoose
 		`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds119548.mlab.com:19548/${process.env.MONGO_DB}`
 	)
 	.then(() => {
-		app.listen(3000);
+		app.listen(8000);
 	})
 	.catch(err => {
 		console.log('Error: ', err);
